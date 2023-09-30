10a1883ebf36a38efe48db25606d92865cd21a78
jeevanaaa <kothapallijeevana7@gmail.com>
2023-09-29 19:23:35 -0500

    adding user service Impl

--- src/main/java/com/mycompany/propertymanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/mycompany/propertymanagement/service/impl/UserServiceImpl.java
@@ -24,7 +24,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO register(UserDTO userDTO) {
-
         Optional<UserEntity> optUe = userRepository.findByOwnerEmail(userDTO.getOwnerEmail());
         if(optUe.isPresent()){
             List<ErrorModel> errorModelList = new ArrayList<>();
